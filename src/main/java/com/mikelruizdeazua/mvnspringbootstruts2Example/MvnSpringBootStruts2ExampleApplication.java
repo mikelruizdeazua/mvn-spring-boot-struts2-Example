806e4ab12cea9244353c96cb0cc1f0fa347fb115
Mikel <mikelruiz@gmail.com>
2020-02-06 16:17:08 +0100

    Default Spring Boot application
    Spring Data + Spring Data JDBC + H2 Database

---
+++ src/main/java/com/mikelruizdeazua/mvnspringbootstruts2Example/MvnSpringBootStruts2ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mikelruizdeazua.mvnspringbootstruts2Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnSpringBootStruts2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnSpringBootStruts2ExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikelruizdeazua/mvnspringbootstruts2Example/MvnSpringBootStruts2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikelruizdeazua.mvnspringbootstruts2Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnSpringBootStruts2ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
